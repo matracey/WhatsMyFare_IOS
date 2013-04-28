87ca490af7dc4c281298cf3332bacdc5c2cd3589
Martin Tracey <mtracey@affinitylv.com>
2013-04-28 17:16:58 +0100

    Added FareResultsViewController class and into storyboard

---
+++ What's My Fare/FareResultsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FareResultsViewController.h
+//  What's My Fare
+//
+//  Created by Martin Tracey on 28/04/2013.
+//  Copyright (c) 2013 Martin Tracey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FareResultsViewController : UIViewController
+
+@end
---
+++ What's My Fare/FareResultsViewController.m
@@ -0,0 +1,38 @@
+//
+//  FareResultsViewController.m
+//  What's My Fare
+//
+//  Created by Martin Tracey on 28/04/2013.
+//  Copyright (c) 2013 Martin Tracey. All rights reserved.
+//
+
+#import "FareResultsViewController.h"
+
+@interface FareResultsViewController ()
+
+@end
+
+@implementation FareResultsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
