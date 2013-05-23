bd5d4df6ffa4b99fce6354ab765a99469c4bbfa5
Martin Tracey <mtracey@affinitylv.com>
2013-05-23 02:32:37 +0100

    Revert "Added lots of stuff to the pickerViewDelegate"
    This reverts commit d290801db8832065a1f88c12a42bf4fb060aa12e.

--- What's My Fare/PickerViewInActionSheetDelegate.h
+++ What's My Fare/PickerViewInActionSheetDelegate.h
@@ -8,24 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-struct PickerLocation{
-    int b;
-    int t;
-};
-
-typedef struct PickerLocation PickerLocation;
-
-PickerLocation PickerLocationMake(int b, int t);
-
-@class PickerViewInActionSheetDelegate;
-
-@protocol PickerViewInActionSheetSenderDelegate <NSObject>
-- (void)didDismissPickerViewWithPickerLocation:(PickerLocation)pickerLoc;
-@end
-
 @interface PickerViewInActionSheetDelegate : NSObject <UIPickerViewDataSource, UIPickerViewDelegate, UIActionSheetDelegate>
-- (void)setPickerViewModel:(NSArray *)pickerViewModel;
-
-- (void)displayPickerViewInView:(UIView *)view; //view can be nil if not iPad
-- (void)getSelectedObjectsAndDismissPickerView;
+- (void)setFareBrackets:(NSArray *)fareBrackets;
 @end
--- What's My Fare/PickerViewInActionSheetDelegate.m
+++ What's My Fare/PickerViewInActionSheetDelegate.m
@@ -8,24 +8,10 @@
 
 #import "PickerViewInActionSheetDelegate.h"
 #import "FareAppDelegate.h"
-#import "PickerViewPopoverViewController.h"
-
-PickerLocation PickerLocationMake(int b, int t){
-    PickerLocation pickerLoc;
-    pickerLoc.b = b;
-    pickerLoc.t = t;
-    return pickerLoc;
-}
 
 @interface PickerViewInActionSheetDelegate()
-@property (weak, nonatomic) NSArray *pickerViewModel;
+@property (weak, nonatomic) NSArray *fareBrackets;
 @property (strong, nonatomic) FareAppDelegate *globalAppProperties;
-@property (strong, nonatomic) id<PickerViewInActionSheetSenderDelegate> delegate;
-@property (strong, nonatomic) NSArray *pickerViewImages;
-
-@property (strong, nonatomic) UIPickerView  *picker;
-@property (strong, nonatomic) UIActionSheet *actionSheet;
-@property (strong, nonatomic) UIPopoverController *popover;
 @end
 
 @implementation PickerViewInActionSheetDelegate
@@ -36,42 +22,12 @@ PickerLocation PickerLocationMake(int b, int t){
     return _globalAppProperties;
 }
 
-- (NSArray *)pickerViewImages
-{
-    if(!_pickerViewImages)
-    {
-        NSMutableArray *arr = [[NSMutableArray alloc] init];
-        for (NSString *s in [self.pickerViewModel objectAtIndex:0]) {
-            UIImage *image = [UIImage imageNamed:[[NSString stringWithFormat:@"%@.png", s] lowercaseString]];
-            if(image) [arr addObject:image];
-        }
-        _pickerViewImages = arr.copy;
-    }
-    return _pickerViewImages;
-}
-
-- (NSArray *)pickerViewModel
-{
-    return  [[NSArray alloc] initWithObjects:
-            [[NSArray alloc] initWithObjects:@"Adult", @"Child", @"Student", nil],
-            [[NSArray alloc] initWithObjects:@"Single", @"Return", nil], nil];
-    //return @[@[@"Adult", @"Child", @"Student"],@[@"Single", @"Return"]];
-}
-
 #pragma mark - Property setters
-- (void)setPickerViewModel:(NSArray *)pickerViewModel
+- (void)setFareBrackets:(NSArray *)fareBrackets
 {
-//    if(_pickerViewModel != pickerViewModel)
-//    {
-//        _pickerViewModel = pickerViewModel;
-//    }
-}
-
-- (void)setDelegate:(id<PickerViewInActionSheetSenderDelegate>)delegate
-{
-    if([delegate conformsToProtocol:@protocol(PickerViewInActionSheetSenderDelegate)])
+    if(_fareBrackets != fareBrackets)
     {
-        _delegate = delegate;
+        _fareBrackets = fareBrackets;
     }
 }
 
@@ -81,42 +37,31 @@ PickerLocation PickerLocationMake(int b, int t){
     //Create label for title
     UILabel *label;
     UIImageView *imgView;
-    UIView *pickerViewRow;
-
-    if(view)
+    
+    if(component == 0)
     {
-        return view;
+        UIImage *image = [UIImage imageNamed:[[NSString stringWithFormat:@"%@.png", [[self.fareBrackets objectAtIndex:0] objectAtIndex:row]] lowercaseString]];
+        //Creating the image view that will hold our fare bracket images
+        imgView = [[UIImageView alloc] initWithFrame:CGRectMake(12, 0, 30, 40)];
+        [imgView setImage:image];
+        label = [[UILabel alloc] initWithFrame:CGRectMake(42, 0, 160, 40)];
+    }else
+    {
+        label = label = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, 160, 40)];
     }
-    else{
-        if(component == 0)
-        {
-            //Creating the image view that will hold our fare bracket images
-            
-            imgView = [[UIImageView alloc] initWithFrame:CGRectMake(12, 0, 30, 40)];
-            [imgView setImage:[self.pickerViewImages objectAtIndex:row]];
-            label = [[UILabel alloc] initWithFrame:CGRectMake(42, 0, 160, 40)];
-        }else
-        {
-            label = label = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, 160, 40)];
-        }
-        
-        label.text = [[self.pickerViewModel objectAtIndex:component] objectAtIndex:row];
-        label.textColor = [self.globalAppProperties.style1 objectForKey:[self.globalAppProperties.styleKeys objectAtIndex:1]];
-        label.font = [self.globalAppProperties.style1 objectForKey:[self.globalAppProperties.styleKeys objectAtIndex:0]];
-        label.backgroundColor = [UIColor clearColor];
-        
-        //Create UIView as a frame for label
-        pickerViewRow = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 160, 40)];
-        [pickerViewRow addSubview:label];
-        if(component == 0) [pickerViewRow addSubview:imgView];
-        return pickerViewRow;
-    }
+    
+    label.text = [[self.fareBrackets objectAtIndex:component] objectAtIndex:row];
+    label.textColor = [self.globalAppProperties.style1 objectForKey:[self.globalAppProperties.styleKeys objectAtIndex:1]];
+    label.font = [self.globalAppProperties.style1 objectForKey:[self.globalAppProperties.styleKeys objectAtIndex:0]];
+    label.backgroundColor = [UIColor clearColor];
+    
+    //Create UIView as a frame for label
+    UIView *pickerViewRow = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 160, 40)];
+    [pickerViewRow addSubview:label];
+    if(component == 0) [pickerViewRow addSubview:imgView];
+    
+    return pickerViewRow;
 }
-
-//- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
-//{
-//    return [[self.pickerViewModel objectAtIndex:component] objectAtIndex:row];
-//}
 
 - (CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
 {
@@ -127,83 +72,14 @@ PickerLocation PickerLocationMake(int b, int t){
 
 - (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
-    return self.pickerViewModel.count;
+    return self.fareBrackets.count;
 }
 
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
-    return [[self.pickerViewModel objectAtIndex:component] count];
+    return [[self.fareBrackets objectAtIndex:component] count];
 }
 
-- (void)displayPickerViewInView:(UIView *)view
-{
-    CGRect pickerFrame = CGRectMake(0.0, 40.0, 320.0, 400.0);
-    UIPickerView *picker = [[UIPickerView alloc] initWithFrame:pickerFrame];
-    picker.showsSelectionIndicator = YES;
-    picker.dataSource = self;
-    picker.delegate = self;
-    
-    //Get the location of the currently selected bracket and scroll to it
-    [self scrollPickerView:picker toPickerLocation:PickerLocationMake(0, 0) animated:NO];
-    
-    //Create and add a done button
-    UISegmentedControl *doneButton = [[UISegmentedControl alloc] initWithItems:@[@"Done"]];
-    doneButton.momentary = YES;
-    doneButton.frame = CGRectMake(260.0, 5.0f, 50.0f, 30.0f);
-    doneButton.segmentedControlStyle = UISegmentedControlStyleBar;
-    doneButton.tintColor = [UIColor blueColor];
-    
-    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-    {
-        UIPopoverController *popover = [[UIPopoverController alloc] initWithContentViewController:[[PickerViewPopoverViewController alloc] initWithPickerView:picker]];
-        PickerViewPopoverViewController *popoverVC = (PickerViewPopoverViewController *)popover.contentViewController;
-        [popoverVC setDoneButton:doneButton fromSender:self];
-        
-        [popover setPopoverContentSize:CGSizeMake(320.0, 220.0)];
-        [popover presentPopoverFromRect:CGRectMake(0.0, 190.0, 320.0, 220.0)
-                                 inView:view
-               permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
-        self.popover = popover;
-        self.picker  = picker;
-    }else{
-        UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Select a Fare Bracket"
-                                                                 delegate:nil
-                                                        cancelButtonTitle:nil
-                                                   destructiveButtonTitle:nil
-                                                        otherButtonTitles:nil];
-        [doneButton addTarget:self action:@selector(getSelectedObjectsAndDismissPickerView) forControlEvents:UIControlEventValueChanged];
-        [actionSheet setActionSheetStyle:UIActionSheetStyleBlackTranslucent];
-        [actionSheet addSubview:picker];
-        [actionSheet addSubview:doneButton];
-        [actionSheet showInView:[[UIApplication sharedApplication] keyWindow]];
-        [actionSheet setBounds:CGRectMake(0.0, 0.0, 320.0, 485.0)];
-        self.actionSheet = actionSheet;
-        self.picker = picker;
-    }
-}
 
-- (void)getSelectedObjectsAndDismissPickerView
-{
-    PickerLocation pickerLoc;
-    if(UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
-    {
-        PickerViewPopoverViewController *popoverVC = (PickerViewPopoverViewController *)self.popover.contentViewController;
-        self.picker = popoverVC.pickerView;
-        pickerLoc = PickerLocationMake([self.picker selectedRowInComponent:0], [self.picker selectedRowInComponent:1]);
-        [self.popover dismissPopoverAnimated:YES];
-    }
-    else
-    {
-        pickerLoc = PickerLocationMake([self.picker selectedRowInComponent:0], [self.picker selectedRowInComponent:1]);
-        [self.actionSheet dismissWithClickedButtonIndex:0 animated:YES];
-    }
-    [self.delegate didDismissPickerViewWithPickerLocation:pickerLoc];
-}
-
-- (void)scrollPickerView:(UIPickerView *)picker toPickerLocation:(PickerLocation)pickerLocation animated:(BOOL)animated
-{
-    [picker selectRow:pickerLocation.b inComponent:0 animated:animated];
-    [picker selectRow:pickerLocation.t inComponent:1 animated:animated];
-}
 
 @end
